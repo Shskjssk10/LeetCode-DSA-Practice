eb372e1d1f6b3aee663baef78b9835705574788e
Shskjssk10 <cadentohjunyi@gmail.com>
2024-08-25 23:38:37 +0800

    Still Attempting Problem 54

--- Arrays & Strings/10_54_spiral_matrix.py
+++ Arrays & Strings/10_54_spiral_matrix.py
@@ -1,3 +1,8 @@
 class Solution:
   def spiralOrder(matrix):
-    return 0
+    height = len(matrix)
+    width = len(matrix[0])
+    if height == 1:
+      return matrix[0]
+  matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+  print(spiralOrder(matrix))
