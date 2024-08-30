2a14582c2657385de1eb30d29605f4c8d3514e6e
Shskjssk10 <cadentohjunyi@gmail.com>
2024-08-30 22:48:37 +0800

    Completed Problem 167

--- 2 Pointers/3_167_two_sum_II.py
+++ 2 Pointers/3_167_two_sum_II.py
@@ -2,12 +2,17 @@ class Solution:
   def twoSum(self, numbers: list[int], target: int) -> list[int]:
     if len(numbers) == 2:
       return [1,2]
-    for first_num in numbers[1:]: 
+    
+    for first_num in numbers: 
       diff = target - first_num
       if diff in numbers: 
-        return [numbers.index(first_num) + 1, numbers.index(diff) + 1]
+        first_index = numbers.index(first_num) + 1
+        return [first_index, numbers[first_index:].index(diff) + first_index + 1]
 
 solution = Solution()
-numbers = [2,3,4]
-target = 6
+numbers = [5,25,75]
+target = 100
 print(solution.twoSum(numbers, target))
+
+## Run Time: 5245ms (Beats 5.03%) 
+## Memory: 17.70mb (Beats 83.57%)
