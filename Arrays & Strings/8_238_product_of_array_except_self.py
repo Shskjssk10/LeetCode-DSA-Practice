8d6dbe97515bfaac4c3ae167eec32255637d6ad1
Shskjssk10 <cadentohjunyi@gmail.com>
2024-09-01 14:11:59 +0800

    Updated Problem 238

--- Arrays & Strings/8_238_product_of_array_except_self.py
+++ Arrays & Strings/8_238_product_of_array_except_self.py
@@ -1,25 +1,20 @@
 class Solution:
   def productExceptSelf(self, nums: list[int]) -> list[int]:
     product = 1
-    got_zero = False
-    multiple_zero = False
-    count = 0 
+    no_of_zero = 0 
     for number in nums:
       if number == 0:
-        got_zero = True
-        count += 1
-        if count == 2:
-          multiple_zero = True
+        no_of_zero += 1
       else:
         product *= number
     for i in range(len(nums)):
-      if multiple_zero: 
+      if no_of_zero > 1: 
         nums[i] = 0
         continue
       if nums[i] == 0:
         nums[i] = int(product / 1)
       else: 
-        if got_zero: 
+        if no_of_zero == 1: 
           nums[i] = 0 
         else: 
           nums[i] = int(product / nums[i])        
